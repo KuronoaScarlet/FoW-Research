ac597e65e73621615d6f1a93ba006ac8a30a1ec0
Ignasi Pardo Carbó <60847464+KuronoaScarlet@users.noreply.github.com>
2021-05-05 17:34:46 +0200

    Approach Notes hechas. Comienza el desarrollo

---
+++ Game/Source/FoW.cpp
@@ -0,0 +1,38 @@
+#include "FoW.h"
+
+#include "App.h"
+#include "Log.h"
+#include "Textures.h"
+#include "Render.h"
+#include "Fonts.h"
+
+FoW::FoW()
+{
+	name.Create("FogOfWar");
+}
+
+bool FoW::Awake()
+{
+	return true;
+}
+
+bool FoW::Start()
+{
+	return true;
+}
+
+bool FoW::Update()
+{
+	return true;
+}
+
+bool FoW::PostUpdate()
+{
+	return true;
+}
+
+bool FoW::CleanUp()
+{
+	return true;
+}
+
--- Game/Source/FoW.h
+++ Game/Source/FoW.h
@@ -3,9 +3,76 @@
 
 #include "Module.h"
 #include "Point.h"
-#include <vector>
-#include <list>
+#include "List.h"
+
+struct FoWTiles
+{
+	iPoint position;
+	int tileHeight, tileWidth;
+
+	enum State
+	{
+		COVERED,
+		TRANSLUCID,
+		UNCOVERED
+	};
+};
+
+class FoW : public Module
+{
+	//Constructor
+	FoW();
+
+	//Destructor
+	virtual ~FoW() {}
+
+	//Module Formal Functions
+	bool Awake();
+	bool Start();
+	bool Update();
+	bool PostUpdate();
+	bool CleanUp();
+
+	/*
+	Función de carga de la niebla.
+		Para cargar la niebla hay que seguir ciertos pasos:
+			1. Guardar todas las tiles en una lista aparte. esta lista debe contener lo siguiente:
+				- Posición de cada tile en el mapa (x, y).
+				- El tamaño de los Tiles.
+				- Tres estados: Cubiertas, translúcidas, descubiertas
+
+			2. Cubrir con Cuadrados o textura (por decidir) todos los tiles del mapa. 
+				- Approaches:
+					· Modificar la función Draw de Map.cpp.
+					· Updatear directamente desde el Update del FoW cada uno de los tiles con un bucle.
+	*/
 
 
+
+
+	/*
+	Función de descarga de la niebla.
+		Sencillamente realizar un CleanUp() de manera satisfactoria. Puedo crear una función UnloadFog para mayor comprensión del módulo.
+	*/
+
+
+
+
+	/*
+	Función para conseguir descubrir los tiles.
+		Buscar el tile del jugador, aplicar un FoV y cambiar los estados de los tiles alrededor del player.
+			1. Función de búsqueda del player en el mapa.
+				- Hacer retrieve del tile en el que se encuentra el Player en todo momento.
+			2. Crear un algoritmo para conocer cuantos tiles hay que mantener decubiertos en función del FoV.
+			3. Conseguir este comportamiento:
+				- COVERED --> UNCOVERED <--> TRANSLUCID.
+	*/
+
+
+
+
+public:
+	List<FoWTiles*> fogTiles;
+};
 
 #endif //_FOW_H_
